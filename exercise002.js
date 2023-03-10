8fb948ce93d28203a8d0d91910a16f6e7c0c7e65
mrunalshri <mrunalyetekar@gmail.com>
2023-03-10 19:48:15 +0000

    completed exercise002: task 1 - converted then catch to async await that shows same response

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,14 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData = async (apiEndPoint) => {
+  try {
+    const result = await fetch(apiEndPoint);
+    const response = await result.json();
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 fetchData(jsonTypicode);
