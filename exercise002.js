598f749d84717ed0c0d56c2a7158363a7a760174
mrunalshri <mrunalyetekar@gmail.com>
2023-03-10 23:34:58 +0000

    completed exercise002: extension - added some new api urls and display response

--- exercise002.js
+++ exercise002.js
@@ -2,6 +2,12 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
+const jsonCatAPI = "https://api.thecatapi.com/v1/images/search";
+
+const jsonFacts = "https://catfact.ninja/fact";
+
+const jsonDogFacts = "https://dog.ceo/api/breeds/image/random";
+
 const fetchData = async (apiEndPoint) => {
   try {
     const result = await fetch(apiEndPoint);
@@ -12,4 +18,19 @@ const fetchData = async (apiEndPoint) => {
   }
 };
 
+const showJSONResponse = async (apiEndPoint) => {
+  try {
+    const result = await fetch(apiEndPoint);
+    console.log(`Response Status Code: ${result.status} , Response Status: ${result.statusText}`);
+    const response = await result.json();
+    console.log("JSON Response:- ", response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 fetchData(jsonTypicode);
+showJSONResponse(jsonCatAPI);
+showJSONResponse(jsonFacts);
+showJSONResponse(jsonDogFacts);
